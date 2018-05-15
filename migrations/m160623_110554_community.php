93e1b92ad93f9a5cb68c570f9da5f185bde2fc35
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-05-15 19:03:10 +0530

    Applied quick fixes.

--- migrations/m160623_110554_community.php
+++ migrations/m160623_110554_community.php
@@ -242,6 +242,7 @@ class m160623_110554_community extends Migration {
 			'label' => $this->string( Yii::$app->core->xxLargeText )->notNull(),
 			'type' => $this->string( Yii::$app->core->mediumText ),
 			'active' => $this->boolean()->defaultValue( false ),
+			'order' => $this->smallInteger( 6 )->defaultValue( 0 ),
 			'valueType' => $this->string( Yii::$app->core->mediumText )->notNull()->defaultValue( Meta::VALUE_TYPE_TEXT ),
 			'value' => $this->text(),
 			'data' => $this->mediumText()
